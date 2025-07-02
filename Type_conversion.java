68f4e04165c78ff110da49a714e6a8eb362a9c07
Rajnish1616 <iamrajnish1616@gmail.com>
2025-07-02 20:51:56 +0530

    Add files via upload

---
+++ Type_conversion.java
@@ -0,0 +1,8 @@
+public class Type_conversion {
+    public static void main(String[] args) {
+        float a = 15;
+        float b = (int)28.8;
+        long c = (int) 23.5;
+        System.out.println(c);
+    }
+}
