887eab25f79ccbbd78c9d2cecd16b0e00e6b5474
Rajnish1616 <iamrajnish1616@gmail.com>
2025-07-13 10:49:50 +0530

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,20 @@
+public class Variable {
+    public static void main(String[] args) {
+        int a = 16;
+        double b = 23.34;
+        int c;
+        c = 23;
+        boolean d = true;
+        float e = 3.4f;
+        String wishes = "Good morning";
+        char my_char = 'A';
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(wishes);
+        System.out.println(my_char);
+    }
+}
